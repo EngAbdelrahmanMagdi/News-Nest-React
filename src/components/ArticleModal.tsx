241913732f3fedee9df43371dcd8b4abf1e68d85
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2025-03-15 05:29:05 +0200

    Article Modal Component

---
+++ src/components/ArticleModal.tsx
@@ -0,0 +1,22 @@
+import { Modal, Button } from "react-bootstrap";
+
+const ArticleModal = ({ modalArticle, setModalArticle }: any) => (
+  <Modal show={!!modalArticle} onHide={() => setModalArticle(null)}>
+    <Modal.Header closeButton>
+      <Modal.Title>{modalArticle?.title}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <p><strong>Category:</strong> {modalArticle?.category.name}</p>
+      <p><strong>Author:</strong> {modalArticle?.author.name}</p>
+      <p><strong>Source:</strong> {modalArticle?.source.name}</p>
+      <p><strong>Published At:</strong> {modalArticle?.published_at}</p>
+      <p><strong>API Source:</strong> {modalArticle?.api_source}</p>
+      <p>{modalArticle?.summary}</p>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={() => setModalArticle(null)}>Close</Button>
+    </Modal.Footer>
+  </Modal>
+);
+
+export default ArticleModal;
