56deafec53706d262726f7795c5cb52ee4fd9afe
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2025-03-15 17:33:33 +0200

    Creating types for Article, ArticleCardProps, ArticleModalProps, ArticleListProps and FilterProps

--- src/types/types.ts
+++ src/types/types.ts
@@ -28,3 +28,43 @@ export interface UserPreferences {
   sources: number[];
   authors: number[];
 }
+
+export interface Article {
+  id: string;
+  title: string;
+  summary: string;
+  url: string;
+  image_url?: string;
+  category: { name: string };
+  author: { name: string };
+  source: { name: string };
+  published_at: string;
+  api_source?: string;
+}
+
+export interface ArticleCardProps {
+  article: Article;
+  setModalArticle: (article: Article | null) => void;
+}
+
+export interface ArticleModalProps {
+  modalArticle: Article | null;
+  setModalArticle: (article: Article | null) => void;
+}
+
+export interface ArticlesListProps {
+  articles: Article[];
+  setModalArticle: (article: Article | null) => void;
+}
+
+export interface FiltersProps {
+  searchTerm: string;
+  setSearchTerm: (value: string) => void;
+  categories: { id: string; name: string }[];
+  setSelectedCategory: (value: string) => void;
+  sources: { id: string; name: string }[];
+  setSelectedSource: (value: string) => void;
+  authors: { id: string; name: string }[];
+  setSelectedAuthor: (value: string) => void;
+  setSelectedDate: (value: string) => void;
+}
